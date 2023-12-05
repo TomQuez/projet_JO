ec157b32bdbc711bc2d4f2f55293b72a58de0aff
TomQuez <tom.quez@live.fr>
2023-12-05 10:04:13 +0100

    ajout models Offre et Blog_article

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from store.models import Offer,Blog_article
+
 # Register your models here.
+admin.site.register(Offer)
+admin.site.register(Blog_article)
--- store/models.py
+++ store/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Offer(models.Model):
+    name=models.CharField(max_length=128)
+    price=models.FloatField(default=0.0)
+    stock=models.IntegerField(default=0)
+    description=models.TextField(blank=True)
+    sales_number=models.IntegerField(default=0)
+    thumbnail=models.ImageField(upload_to="offer_images",blank=True, null=True)
+    
+    def __str__(self):
+        return f'{self.name} ({self.stock})'
+    
+class Blog_article(models.Model):
+    title=models.CharField(max_length=200)
+    description=models.TextField()
+    thumbnail=models.ImageField(upload_to="blog_article_images",blank=True,null=True)
